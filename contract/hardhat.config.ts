2118b3c5b51bb07457d16c2e3e79e7c1f3feed7d
Jeremiah Samuel <livinalt@gmail.com>
2024-02-22 10:14:57 +0100

    dapp integration completed

--- contract/hardhat.config.ts
+++ contract/hardhat.config.ts
@@ -15,6 +15,3 @@ export default config;
 
 
 
-
-const URL = "https://eth-sepolia.g.alchemy.com/v2/IrKacA794mfjNeuRwjIHcDdlrcJBiekz";
-const KEY = "8fd68b2aa1e7719bf2f5bfb9ea7004314b594b0b558e3c7327b0f3ba604a11fe" ;
